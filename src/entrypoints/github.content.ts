8e8392febdbc3e41bab042d71fb5f445f7e2a6b2
wlft <100052562+wlft@users.noreply.github.com>
2026-04-05 04:52:13 +1000

    feat(icons): .ignore

--- src/entrypoints/github.content.ts
+++ src/entrypoints/github.content.ts
@@ -119,6 +119,7 @@ export default defineContentScript({
             'cnfg': 'config.svg',
             'conf': 'config.svg',
             'ini': 'config.svg',
+            'ignore': 'config.svg',
             'version*': 'config.svg',
             'revision*': 'config.svg',
             'yapf': 'config.svg',
