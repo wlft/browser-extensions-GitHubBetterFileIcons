e9ff059714c79338676942091d93738476f803f5
wlft <100052562+wlft@users.noreply.github.com>
2026-06-20 00:05:59 +1000

    feat(icons): sqlite

--- src/entrypoints/github.content.ts
+++ src/entrypoints/github.content.ts
@@ -138,6 +138,7 @@ export default defineContentScript({
             'sh': 'bash.svg',
 
             'sql': 'sql.svg',
+            'sqlite': 'sqlite-lighter.svg',
 
             'lefthook*': 'lefthook.svg',
 
